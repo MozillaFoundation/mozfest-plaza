ac61be6ea43dacae7e1633ba3ee8949a9106ecf3
Rob Anderson <robanderson@hey.com>
2023-02-27 16:21:28 +0000

    feat: add cloudinary share URLs

--- server/src/general/general-router.ts
+++ server/src/general/general-router.ts
@@ -1,6 +1,7 @@
 import KoaRouter from '@koa/router'
 import escapeHtml from 'escape-html'
 import dedent from 'dedent'
+import cloudinary from 'cloudinary'
 
 import { validateStruct, VOID_RESPONSE } from '@openlab/deconf-api-toolkit'
 import { SessionState } from '@openlab/deconf-shared'
@@ -40,6 +41,18 @@ export class GeneralRouter implements AppRouter {
       ctx.redirect(new URL(this.#context.env.CLIENT_URL).toString())
     })
 
+    const shareDateFmt = new Intl.DateTimeFormat('en', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+      timeZone: 'Europe/Amsterdam',
+    })
+    const shareTimeFmt = new Intl.DateTimeFormat('en', {
+      timeStyle: 'short',
+      timeZone: 'Europe/Amsterdam',
+      hourCycle: 'h24',
+    })
+
     // NOTE: Create "share-router" if more shares are needed
     router.get(
       'general.shareSession',
@@ -47,13 +60,14 @@ export class GeneralRouter implements AppRouter {
       async (ctx) => {
         const { sessionId } = validateStruct(ctx.params, SessionIdStruct)
         const session = await this.#confRepo.findSession(sessionId)
+        const slots = await this.#confRepo.getSlots()
 
         const appTitle = 'Mozilla Festival 2023'
         const pageTitle = session?.title.en
         const pageDescription = session?.content.en || ''
-        const sessionImage = this.#url
-          .getClientAsset('opengraph-session.png')
-          .toString()
+        const slot = session?.slot
+          ? slots.find((s) => s.id === session.slot)
+          : null
 
         // Return a http 404 if the session doesn't exist, isn't confirmed
         // or doesn't have an English title
@@ -71,7 +85,59 @@ export class GeneralRouter implements AppRouter {
         const sessionUrl = this.#url.getSessionLink(session.id)
         sessionUrl.searchParams.set('ref', 'share')
 
+        const shortTitle = pageTitle.substring(0, 100)
         const shortDesc = pageDescription.substring(0, 200)
+
+        const transformation: any[] = [
+          {
+            width: 800,
+            crop: 'scale',
+          },
+          {
+            color: '#FFFFFF',
+            width: 750,
+            overlay: {
+              font_family: 'mlilnejuqzzv1rz4wlkh.ttf',
+              font_weight: 'bold',
+              font_size: 50,
+              text: shortTitle,
+            },
+          },
+          {
+            flags: 'layer_apply',
+            gravity: 'west',
+            x: 25,
+            y: 100,
+          },
+        ]
+
+        if (slot) {
+          transformation.push(
+            {
+              color: '#FFFFFF',
+              overlay: {
+                font_family: 'mlilnejuqzzv1rz4wlkh.ttf',
+                font_weight: 'normal',
+                font_size: 25,
+                text:
+                  shareTimeFmt.format(slot.start) +
+                  ' CET     ' +
+                  shareDateFmt.format(slot.start),
+              },
+            },
+            {
+              flags: 'layer_apply',
+              gravity: 'west',
+              x: 25,
+              y: 50,
+            }
+          )
+        }
+
+        const imageUrl = cloudinary.v2.url('mozfest_og_bg_cm5nxq.jpg', {
+          cloud_name: 'djjkljkg7',
+          transformation,
+        })
 
         const head = dedent`
           <meta charset="utf-8">
@@ -82,7 +148,7 @@ export class GeneralRouter implements AppRouter {
           <meta property="og:type" content="website">
           <meta property="og:site_name" content="${appTitle}">
           <meta property="og:url" content="${sessionUrl}">
-          <meta property="og:image" content="${sessionImage}">
+          <meta property="og:image" content="${imageUrl}">
           <meta http-equiv="refresh" content="0; url='${sessionUrl}'" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@mozillafestival" />
