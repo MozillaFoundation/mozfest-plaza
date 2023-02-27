be0fd3617ac0733a1e1b12e946b6cda7f8e7b20a
Rob Anderson <robanderson@hey.com>
2023-02-27 16:27:05 +0000

    chore: add code note

--- client/src/lib/schedule.ts
+++ client/src/lib/schedule.ts
@@ -5,6 +5,7 @@ import { ExtraRoutes } from './module'
 
 /** Work out which page to return to from a given `Session` */
 export function getSessionParentRoute(session: Session): Location {
+  // TODO: update to use `sessionTypeIds` if the back-button is unhidden
   if (session.type === 'art-and-media') return { name: ExtraRoutes.Arts }
   if (session.type === 'fringe-events') return { name: ExtraRoutes.Fringe }
   if (session.type === 'mozfest-house') return { name: ExtraRoutes.House }
