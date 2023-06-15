5b97dbe14020ed4fecd60557c9e41c5eced704e6
Rob Anderson <robanderson@hey.com>
2023-06-15 16:38:12 +0100

    fix: remove house-info gate

--- client/src/views/HouseInfoView.vue
+++ client/src/views/HouseInfoView.vue
@@ -5,17 +5,16 @@
 <script lang="ts">
 import Vue from 'vue'
 import ContentLayout from '@/components/MozContentLayout.vue'
-import { ExtraRoutes, mapApiState } from '@/lib/module'
+import { mapApiState } from '@/lib/module'
+import { guardPage } from '@openlab/deconf-ui-toolkit'
 
 export default Vue.extend({
   components: { ContentLayout },
   computed: {
-    ...mapApiState('api', ['schedule']),
+    ...mapApiState('api', ['schedule', 'user']),
   },
   mounted() {
-    if (this.schedule?.settings.house?.visible) {
-      this.$router.replace({ name: ExtraRoutes.House })
-    }
+    guardPage(this.schedule?.settings.house, this.user, this.$router)
   },
 })
 </script>
