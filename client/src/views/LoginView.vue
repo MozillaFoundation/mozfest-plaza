b6ad78e51710904d8bf88a6e49d5c32d9621b44d
Rob Anderson <rob@andrsn.uk>
2024-10-17 17:21:13 +0100

    add google auth UI

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -9,6 +9,20 @@
           {{ $t('deconf.login.doneText') }}
         </p>
       </template>
+      <template v-slot:extraOptions>
+        <div class="content loginView-oauth">
+          <hr />
+          <p>{{ $t('mozfest.login.oauth') }}</p>
+          <div class="buttons">
+            <a :href="googleLink" class="button is-google">
+              <span class="icon">
+                <FontAwesomeIcon :icon="['fab', 'google']" />
+              </span>
+              <span> {{ $t('mozfest.login.google') }} </span>
+            </a>
+          </div>
+        </div>
+      </template>
     </LoginView>
   </MozUtilLayout>
 </template>
@@ -18,8 +32,21 @@ import { defineComponent } from 'vue'
 
 import MozUtilLayout from '@/components/MozUtilLayout.vue'
 import { LoginView, ApiContent } from '@openlab/deconf-ui-toolkit'
+import { env } from '@/plugins/env-plugin'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export default defineComponent({
-  components: { MozUtilLayout, LoginView, ApiContent },
+  components: { MozUtilLayout, LoginView, ApiContent, FontAwesomeIcon },
+  computed: {
+    googleLink(): string {
+      return new URL('./auth/oauth2/google', env.SERVER_URL).toString()
+    },
+  },
 })
 </script>
+
+<style lang="scss">
+.loginView-oauth {
+  margin-block-start: 3em;
+}
+</style>
