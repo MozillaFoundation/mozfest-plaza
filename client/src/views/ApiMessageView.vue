2944b759037ff40eb393755cd8749b7fd3d71396
Rob Anderson <rob@andrsn.uk>
2024-10-17 17:11:35 +0100

    add extra api messages

--- client/src/views/ApiMessageView.vue
+++ client/src/views/ApiMessageView.vue
@@ -1,18 +1,42 @@
 <template>
   <MozUtilLayout>
-    <ApiMessage :error-code="errorCode" />
+    <div v-if="errorCode === 'oauth2_failed'" class="content">
+      <h1>{{ $t('mozfest.apiMessage.oauth2Failed.title') }}</h1>
+      <p>{{ $t('mozfest.apiMessage.oauth2Failed.info') }}</p>
+      <p>
+        <router-link :to="loginRoute" class="button is-link">
+          {{ $t('mozfest.apiMessage.oauth2Failed.action') }}
+        </router-link>
+      </p>
+    </div>
+    <div v-else-if="errorCode === 'not_registered'" class="content">
+      <h1>{{ $t('mozfest.apiMessage.notRegistered.title') }}</h1>
+      <p>{{ $t('mozfest.apiMessage.notRegistered.info') }}</p>
+      <p>
+        <router-link :to="loginRoute" class="button is-link">
+          {{ $t('mozfest.apiMessage.notRegistered.action') }}
+        </router-link>
+      </p>
+    </div>
+    <ApiMessage v-else :error-code="errorCode" />
   </MozUtilLayout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import MozUtilLayout from '@/components/MozUtilLayout.vue'
-import { ApiMessage } from '@openlab/deconf-ui-toolkit'
+import { ApiMessage, Routes } from '@openlab/deconf-ui-toolkit'
+import type { RouteLocationRaw } from 'vue-router'
 
 export default defineComponent({
   components: { MozUtilLayout, ApiMessage },
   props: {
     errorCode: { type: String, default: 'unknown' },
+  },
+  computed: {
+    loginRoute(): RouteLocationRaw {
+      return { name: Routes.Login }
+    },
   },
 })
 </script>
