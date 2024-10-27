ff87e04d67636b9d97453496cf7949e2c607c668
Rob Anderson <rob@andrsn.uk>
2024-10-27 12:50:43 +0000

    work on calendar sync

--- server/src/commands/calendar-sync-command.ts
+++ server/src/commands/calendar-sync-command.ts
@@ -1,26 +1,20 @@
+import crypto from 'node:crypto'
+
 import {
   ConferenceRepository,
-  KeyValueService,
   localise,
   PostgresClient,
   PostgresService,
   RegistrationRepository,
   UrlService,
 } from '@openlab/deconf-api-toolkit'
-import { createDebug, createEnv, EnvRecord } from '../lib/module.js'
-import { Oauth2Record, Oauth2Repository } from '../deconf/oauth2-repository.js'
-import {
-  Attendance,
-  Localised,
-  Registration,
-  Session,
-  SessionSlot,
-} from '@openlab/deconf-shared'
-import { pickAStore } from './serve-command.js'
-import { getGoogleClient, GOOGLE_CALENDAR_SCOPE } from '../lib/google.js'
-import { Credentials, GoogleAuth, OAuth2Client } from 'google-auth-library'
+import { Attendance, Localised, Registration } from '@openlab/deconf-shared'
 import { calendar_v3, google } from 'googleapis'
-import crypto from 'node:crypto'
+
+import { Oauth2Record, Oauth2Repository } from '../deconf/oauth2-repository.js'
+import { getGoogleClient, GOOGLE_CALENDAR_SCOPE } from '../lib/google.js'
+import { createDebug, createEnv, EnvRecord } from '../lib/module.js'
+import { pickAStore } from './serve-command.js'
 
 const debug = createDebug('cmd:dev-auth')
 
@@ -32,6 +26,7 @@ function getTokens(pg: PostgresClient) {
   `
 }
 
+/** Group OAuth2 records by attendee */
 function perAttendee(tokens: Oauth2Record[]) {
   const map = new Map<number, Oauth2Record[]>()
   for (const token of tokens) {
@@ -43,6 +38,7 @@ function perAttendee(tokens: Oauth2Record[]) {
   return map
 }
 
+/** Get the attendance records for an attendee */
 function getAttendance(pg: PostgresClient, attendee: number) {
   return pg.sql<Attendance>`
     SELECT id, created, attendee, session
@@ -51,18 +47,22 @@ function getAttendance(pg: PostgresClient, attendee: number) {
   `
 }
 
+/** From a set of attendee tokens, get their latest access & refresh tokens */
 export function getActiveToken(tokens: Oauth2Record[]) {
-  const filtered = tokens
+  const newestFirst = tokens
     .filter((t) => t.scope.includes(GOOGLE_CALENDAR_SCOPE))
     .sort((a, b) => b.created.getTime() - a.created.getTime())
 
-  if (filtered.length === 0) return null
+  if (newestFirst.length === 0) return null
 
-  const refreshToken = filtered.map((t) => t.refreshToken).filter((t) => t)[0]
-  const accessToken = filtered[0].accessToken
-  return { refreshToken, accessToken }
+  // Return the newest token that has a refresh token & latest access token
+  return {
+    refreshToken: newestFirst.map((t) => t.refreshToken).filter((t) => t)[0],
+    accessToken: newestFirst[0].accessToken,
+  }
 }
 
+/** An unlocalised event */
 interface GlobalEvent {
   id: string
   title: Localised
@@ -72,6 +72,7 @@ interface GlobalEvent {
   end: Date
 }
 
+/** Get all events for the conference, mapped by id */
 async function getEvents(
   repo: ConferenceRepository,
   url: UrlService
@@ -87,7 +88,7 @@ async function getEvents(
     if (!slot) continue
 
     events.set(session.id, {
-      id: session.id,
+      id: getGoogleId(session.id),
       title: session.title,
       location: url.getSessionLink(session.id).toString(),
       description: session.content,
@@ -98,6 +99,7 @@ async function getEvents(
   return events
 }
 
+/** An event to be synchronised with the users calendar */
 interface CalendarEvent {
   id: string
   title: string
@@ -107,6 +109,7 @@ interface CalendarEvent {
   end: Date
 }
 
+/** Get an attendee's events to synchronise with Google */
 async function getUserEvents(
   pg: PostgresClient,
   registration: Registration,
@@ -123,47 +126,111 @@ async function getUserEvents(
     }))
 }
 
+/** Get the events already in a user's google calendar */
 async function getGoogleEvents(
   gcal: calendar_v3.Calendar,
-  calendarId: string
-): Promise<Map<string, CalendarEvent>> {
-  let items: calendar_v3.Schema$Events[] = []
-  const events = new Map<string, CalendarEvent>()
+  calendarId?: string
+): Promise<CalendarEvent[]> {
+  if (!calendarId) return []
+
+  const events: CalendarEvent[] = []
   try {
-    // let pageToken: string | undefined = undefined
+    let res = await gcal.events.list({ calendarId /*, pageToken */ })
 
-    // while (true) {
-    const res = await gcal.events.list({ calendarId /*, pageToken */ })
-
-    for (const event of res.data.items ?? []) {
-      if (
-        !event.id ||
-        !event.summary ||
-        !event.start?.dateTime ||
-        !event.end?.dateTime
-      ) {
-        continue
+    while (true) {
+      for (const event of res.data.items ?? []) {
+        if (
+          !event.id ||
+          !event.summary ||
+          !event.start?.dateTime ||
+          !event.end?.dateTime
+        ) {
+          continue
+        }
+        events.push({
+          id: event.id,
+          title: event.summary,
+          location: event.location ?? '',
+          description: event.description ?? '',
+          start: new Date(event.start.dateTime),
+          end: new Date(event.end.dateTime),
+        })
       }
-      events.set(event.id, {
-        id: event.id,
-        title: event.summary,
-        location: event.location ?? '',
-        description: event.description ?? '',
-        start: new Date(event.start.dateTime),
-        end: new Date(event.end.dateTime),
+
+      if (!res.data.nextPageToken) break
+
+      res = await gcal.events.list({
+        calendarId,
+        pageToken: res.data.nextPageToken,
       })
     }
 
-    // TODO: paginate...
-    // NOTE: we can set the event id to the session ID !
-
-    //   pageToken = res.data.nextPageToken
-    // }
-
     return events
   } catch {
-    return new Map()
+    return []
   }
+}
+
+function hashEvent(
+  event: CalendarEvent,
+  cache: WeakMap<CalendarEvent, string>
+) {
+  if (cache.has(event)) return cache.get(event)!
+
+  const hash = crypto
+    .createHash('sha256')
+    .update(JSON.stringify(event))
+    .digest('base64')
+
+  cache.set(event, hash)
+
+  return hash
+}
+
+/**
+ * Google takes a base32 string as the identifier.
+ * This returns a base16 to satisfy that without adding any extra NPM deps
+ */
+function getGoogleId(input: string) {
+  return Buffer.from(input).toString('hex')
+}
+
+interface CalendarDiff {
+  missing: CalendarEvent[]
+  removed: CalendarEvent[]
+  modified: CalendarEvent[]
+}
+
+function diffEvents(
+  local: Map<string, CalendarEvent>,
+  remote: Map<string, CalendarEvent>
+): CalendarDiff {
+  const hashes = new WeakMap<CalendarEvent, string>()
+
+  // Find events that are not in the remote, so need creating
+  const missing: CalendarEvent[] = []
+  for (const l of local.values()) {
+    if (!remote.has(l.id)) missing.push(l)
+  }
+
+  // Find events that are no longer in local, so need removing
+  const removed: CalendarEvent[] = []
+  for (const r of remote.values()) {
+    if (!local.has(r.id)) removed.push(r)
+  }
+
+  // Find events that have been modified since uploading, so need updating
+  const modified: CalendarEvent[] = []
+  for (const l of local.values()) {
+    const r = remote.get(l.id)
+    if (!r) continue
+
+    if (hashEvent(l, hashes) !== hashEvent(r, hashes)) {
+      modified.push(l)
+    }
+  }
+
+  return { missing, removed, modified }
 }
 
 interface CalendarToken {
@@ -200,6 +267,15 @@ function setupAuth(
   return auth
 }
 
+function keyedBy<T, K extends keyof T>(key: K, items: T[]) {
+  return new Map(items.map((i) => [i[key], i]))
+}
+
+interface CalendarUserData {
+  googleCalendarId: string | undefined | null
+  googleCalendarDate: string | undefined
+}
+
 export interface CalendarSyncCommandOptions {
   dryRun: boolean
 }
@@ -232,11 +308,12 @@ export async function calendarSyncCommand(options: CalendarSyncCommandOptions) {
         debug('[skip] not verified')
         continue
       }
-      const userData = registration.userData as Record<'googleCalendarId', any>
+      const userData = registration.userData as CalendarUserData
 
       // Skip the calendar sync if explicitly set to null
       if (userData.googleCalendarId === null) {
         debug('[skip] sync disabled')
+        continue
       }
 
       // Don't run if there is no calendar token
@@ -247,56 +324,127 @@ export async function calendarSyncCommand(options: CalendarSyncCommandOptions) {
       }
 
       // Fetch the user's attendance and map those to sessions
-      const localEvents = await getUserEvents(pg, registration, allEvents)
-      debug(
-        'attending %o',
-        localEvents.map((s) => s.id)
+      const localEvents = keyedBy(
+        'id',
+        await getUserEvents(pg, registration, allEvents)
       )
+      debug('attending %o', [...localEvents.keys()])
 
-      // Create a google client to authenticate the user
+      // Create a google client authenticated with the user
       const auth = setupAuth(env, activeToken, attendee, enqueued, oauthRepo)
       const gcal = google.calendar({ version: 'v3', auth })
 
-      // const calendar = await upsertCalendar(gcal, userData.googleCalendarId)
-
-      // if (!calendar) {
-      //   debug('[skip] failed to upsert calendar')
-      //   continue
-      // }
-
-      // await storeCalendarId(pg, attendee, calendar.id!)
-
-      // const events = await gcal.events.list({
-      //   calendarId: calendar?.id!,
-      // })
-
-      const googleEvents = await getGoogleEvents(
-        gcal,
-        userData.googleCalendarId
+      // Fetch the user's google events
+      const googleEvents = keyedBy(
+        'id',
+        await getGoogleEvents(gcal, userData.googleCalendarId!)
       )
 
-      console.log(googleEvents)
+      const diff = diffEvents(localEvents, googleEvents)
 
-      //
+      if (options.dryRun) {
+        console.log('user data', userData)
+        console.log('diff', diff)
+        continue
+      }
+
+      const calendarId = await upsertCalendar(gcal, userData.googleCalendarId!)
+      if (!calendarId) {
+        debug('[skip] user deleted calendar')
+        userData.googleCalendarId = null
+        await updateUserData(pg, attendee, userData)
+        continue
+      }
+
+      debug('storing calendar id')
+      userData.googleCalendarId = calendarId
+      await updateUserData(pg, attendee, userData)
+
+      try {
+        debug('starting diff')
+        await performDiff(gcal, calendarId, diff)
+      } catch (error) {
+        console.error('failed to perform diff', error)
+        continue
+      }
+
+      debug('storing calendar date')
+      userData.googleCalendarDate = new Date().toISOString()
+      await updateUserData(pg, attendee, userData)
+
+      debug('done')
     }
   })
 
+  debug('waiting for queue')
   await Promise.all(enqueued)
 
   await postgres.close()
   await store.close()
+
+  debug('finished')
 }
 
-function diffEvents(local: CalendarEvent[], remote: CalendarEvent[]) {
-  // work out new, updated, missing
+function googleEvent(event: CalendarEvent) {
+  return {
+    summary: event.title,
+    description: event.description,
+    start: { dateTime: event.start.toISOString() },
+    end: { dateTime: event.end.toISOString() },
+    location: event.location,
+  }
 }
 
-async function storeCalendarId(
+async function performDiff(
+  gcal: calendar_v3.Calendar,
+  calendarId: string,
+  diff: CalendarDiff
+) {
+  // https://developers.google.com/calendar/api/v3/reference/events/insert
+  for (const event of diff.missing) {
+    debug('insert', event.id)
+    await gcal.events.insert({
+      calendarId,
+      requestBody: {
+        id: event.id,
+        ...googleEvent(event),
+      },
+    })
+  }
+
+  // https://developers.google.com/calendar/api/v3/reference/events/update
+  for (const event of diff.modified) {
+    debug('update', event.id)
+    await gcal.events.update({
+      eventId: event.id,
+      calendarId,
+      requestBody: { ...googleEvent(event) },
+    })
+  }
+
+  for (const event of diff.removed) {
+    debug('delete', event.id)
+    await gcal.events.delete({
+      eventId: event.id,
+      calendarId,
+    })
+  }
+
+  if (
+    diff.missing.length === 0 &&
+    diff.modified.length == 0 &&
+    diff.removed.length === 0
+  ) {
+    debug('no changes')
+  }
+}
+
+async function updateUserData(
   pg: PostgresClient,
   attendee: number,
-  calendarId: string
+  { googleCalendarDate, googleCalendarId }: CalendarUserData
 ) {
-  const patch = JSON.stringify({ googleCalendar: calendarId })
+  const patch = JSON.stringify({ googleCalendarDate, googleCalendarId })
   return pg.sql`
     UPDATE attendees
     SET "userData" = "userData" || ${patch}
@@ -304,19 +452,30 @@ async function storeCalendarId(
   `
 }
 
-async function upsertCalendar(gcal: calendar_v3.Calendar, calendarId?: string) {
+async function upsertCalendar(
+  gcal: calendar_v3.Calendar,
+  calendarId?: string
+): Promise<string | null> {
   if (calendarId) {
-    const res = await gcal.calendars.get({ calendarId }) //.catch(() => null)
-    if (res?.data) return res.data
+    const res = await gcal.calendars.get({ calendarId }).catch(() => null)
+    if (res?.data?.id) {
+      debug('found calendar')
+      return res.data.id
+    } else {
+      debug('calendar deleted')
+      return null
+    }
   }
 
-  const calendar = await gcal.calendars.insert({
-    quotaUser: 'mozfest-development',
-    requestBody: {
-      summary: 'MozFest calendar',
-    },
-  })
-  // .catch(() => null)
+  debug('creating google calendar')
+  const calendar = await gcal.calendars
+    .insert({
+      quotaUser: 'mozfest-development',
+      requestBody: {
+        summary: 'MozFest calendar',
+      },
+    })
+    .catch(() => null)
 
-  return calendar?.data
+  return calendar?.data.id ?? null
 }
